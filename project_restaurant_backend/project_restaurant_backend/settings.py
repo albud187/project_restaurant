23fa2e2f1da8a5305d7d4cbfe25584d72097e885
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-12-03 17:17:12 -0400

    update

--- project_restaurant_backend/project_restaurant_backend/settings.py
+++ project_restaurant_backend/project_restaurant_backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_restaurants_auth',
+    'app_menu_orders'
 ]
 
 MIDDLEWARE = [
