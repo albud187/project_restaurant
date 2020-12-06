432de05bc772dfb9ba98005cccc9a101cf5f2804
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2020-12-06 10:57:58 -0400

    display restaurants

--- project_restaurant_frontend/src/components/Restaurants.js
+++ project_restaurant_frontend/src/components/Restaurants.js
@@ -2,33 +2,119 @@ import React from 'react';
 
 import { List} from 'antd';
 import { Card } from 'antd';
+import { Table, Tag, Space } from 'antd';
 
 import * as API_PATHS from '../api_path.js'
 const API_PATH = API_PATHS.API_PATH
+const { Column, ColumnGroup } = Table;
+
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+  },
+  {
+    title: 'Type',
+    dataIndex: 'type',
+    key: 'type',
+  },
+  {
+    title: 'City',
+    dataIndex: 'city',
+    key: 'city',
+  },
+
+  {
+    title: 'Province',
+    dataIndex: 'state_province',
+    key: 'province',
+  },
+
+  {
+    title: 'Hours',
+    dataIndex: 'hours',
+    key: 'hours',
+  },
+
+  {
+    title: 'Menu',
+    dataIndex: 'menu',
+    key: 'menu',
+  },
+]
 
 const Restaurants =(props)=>{
 
   return(
-
+<div>
   <List
-    grid={{ gutter: 16, column: 4}}
 
     dataSource={props.data}
 
     renderItem={item => (
       <List.Item key={item.id}>
-      <Card>
+
         <List.Item.Meta
-          title={<a href={"/restaurant/"+`${item.id}`}>
+          title={<a href={`/restaurant/${item.id}`}>
           {item.id} - {item.name}
           </a>}/>
 
         <p><strong>contact</strong> : {item.public_email}, {item.public_phonenumber}</p>
 
-        </Card>
+
       </List.Item>
     )}
   />
+
+<Table dataSource={props.data}
+
+columns ={
+  [
+    {
+      title: 'Name',
+      dataIndex: 'id',
+      key: 'id',
+      render: item => (
+        <a href={`/restaurant/${item}`}>
+        {item}
+        </a>
+      )
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type',
+    },
+    {
+      title: 'City',
+      dataIndex: 'city',
+      key: 'city',
+    },
+
+    {
+      title: 'Province',
+      dataIndex: 'state_province',
+      key: 'province',
+    },
+
+    {
+      title: 'Hours',
+      dataIndex: 'hours',
+      key: 'hours',
+    },
+
+    {
+      title: 'Menu',
+      dataIndex: 'menu',
+      key: 'menu',
+    },
+  ]
+}>
+
+</Table>
+</div>
   )
 }
 
