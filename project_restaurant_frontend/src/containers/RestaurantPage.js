6f58868d759894aa079c3b53108fe4c8e4054ed8
Alfa Budiman <63726302+albud187@users.noreply.github.com>
2021-01-03 11:56:30 -0400

    trying to render item cost from axios calls

--- project_restaurant_frontend/src/containers/RestaurantPage.js
+++ project_restaurant_frontend/src/containers/RestaurantPage.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { List} from 'antd';
 import {Button} from 'antd'
-
 import Menu from '../components/Menu.js'
 import * as API_PATHS from '../api_path.js'
 
 const API_PATH = API_PATHS.API_PATH
 
+function to_array(input_obj){
+  var output_array = [];
+  var x;
+  for (x of Object.keys(input_obj)){
+    output_array.push({"id":x,"quantity":input_obj[x]})
+  };
+  return(output_array)
+}
 
+async function get_cost(order_id){
+  let data = await axios.get(`${API_PATH}api/MenuItems/${order_id}/`)
+  var output = JSON.stringify(data['data']['price'])
+  console.log(output + ' is the cost')
+
+}
 class RestaurantPage extends Component {
 
   state = {
@@ -32,6 +45,12 @@ class RestaurantPage extends Component {
     })
   }
 
+  whatispromise =(order_id)=>{
+    console.log(axios.get(`${API_PATH}api/MenuItems/${order_id}/`))
+    console.log(Object.keys(axios.get(`${API_PATH}api/MenuItems/${order_id}/`)))
+    console.log('promise is')
+  }
+
   fetchMenus = (restaurantID) =>{
     axios.get(`${API_PATH}api/restaurant_menus?restaurantID=${restaurantID}`)
     .then(result=>{
@@ -44,17 +63,17 @@ class RestaurantPage extends Component {
   }
 
   handleOrderSubmit = (event) =>{
-    //axios.post new order
+    //turns localStorage into a list of items to order with cost and everything
   }
 
 
 
 componentDidMount(){
-
+  this.whatispromise(1)
   const restaurantID = this.props.match.params.restaurantID;
   this.fetchRestaurant(restaurantID);
   this.fetchMenus(restaurantID);
-
+  this.whatispromise(1)
 }
 
 
@@ -65,10 +84,8 @@ componentDidMount(){
       <h1>Restaurant Page Here</h1>
       <p>id = {this.props.match.params.restaurantID}</p>
 
-      <h1>state info </h1>
       <p>restaurant name = {this.state.restaurant.name}</p>
       <p>restaurant address = {this.state.restaurant.street_address}</p>
-
 
       <List
         dataSource={this.state.menus}
